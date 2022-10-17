a2da589a094c6a0416b974b1f01a34e1e1b93995
Vinh Nguyen <vinh_nguyen1211@yahoo.com.vn>
2022-10-17 21:22:01 +0700

    Remove unused button in storybook

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+// import React from 'react';
+// import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Button } from './Button';
+// import { Button } from './Button';
 
-export default {
-  title: 'ROQ/Button',
-  component: Button,
-  parameters: {
-    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
-  },
-  // decorators: [AppDecorators]
-} as ComponentMeta<typeof Button>;
+// export default {
+//   title: 'ROQ/Button',
+//   component: Button,
+//   parameters: {
+//     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+//     layout: 'fullscreen',
+//   },
+//   // decorators: [AppDecorators]
+// } as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} >Button</Button>;
+// const Template: ComponentStory<typeof Button> = (args) => <Button {...args} >Button</Button>;
 
-export const Primary = Template.bind({});
-export const Secondary = () => <Button>Button</Button>
+// export const Primary = Template.bind({});
+// export const Secondary = () => <Button>Button</Button>
--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -5,7 +5,7 @@ import AppDecorators from '../../../.storybook/decorators'
 import { Card } from './Card';
 
 export default {
-  title: 'ROQ/Card',
+  title: 'Roq Widgets/Card',
   component: Card,
   argTypes: {
     title: {
