89e59ae8a654aa9fe433b60026604393b89e6ee2
Hlib Lozhkovyi <hlib.lozhkovyi@gmail.com>
2022-10-18 15:47:33 +0300

    CPD-1460 multisource poc

--- src/components/message-center/conversation-card/conversation-card.tsx
+++ src/components/message-center/conversation-card/conversation-card.tsx
@@ -1,12 +1,10 @@
-import './conversation-card.css';
+import clsx from "classnames";
+import React, { CSSProperties, ReactElement } from "react";
 
-import clsx from 'classnames';
-import React, { Element } from 'react';
+import { AvatarGroup } from "../../common/avatar-group/avatar-group";
+import { StackedText } from "../../common/stacked-text/stacked-text";
 
-import { AvatarGroup } from '../../common/avatar-group/avatar-group';
-import { StackedText } from '../../common/stacked-text/stacked-text';
-
-const _CLASS_IS = 'roq-widget-' + 'conversation-card';
+const _CLASS_IS = "roq-widget-" + "conversation-card";
 
 interface ConversationCardProps {
   title: string;
@@ -14,13 +12,20 @@ interface ConversationCardProps {
   message: string;
   members: any[];
   className?: string;
+  styles?: CSSProperties;
   classNames?: {
     container?: string;
+    inner?: string;
+    top?: string;
+    title?: string;
     message?: string;
   };
   components?: {
-    container: Element;
-    message: Element;
+    Container?: ReactElement;
+    Inner?: ReactElement;
+    Top?: ReactElement;
+    Title?: ReactElement;
+    Message?: ReactElement;
   };
 }
 
@@ -30,23 +35,35 @@ export const ConversationCard = (props: ConversationCardProps) => {
     date,
     message,
     members,
+    style,
     className,
     classNames,
-    components = { container: 'div', message: 'p' },
+    components,
   } = props;
 
-  const Container = components?.container ?? 'div';
-  const Inner = components?.inner ?? 'div';
-  const Message = components?.message ?? 'p';
+  const Container = components?.Container ?? "div";
+  const Inner = components?.Inner ?? "div";
+  const Top = components?.Top ?? "div";
+  const Title = components?.Title ?? StackedText;
+  const Message = components?.Message ?? "p";
 
   return (
-    <Container className={clsx(_CLASS_IS, className, classNames?.container)}>
-      <Inner>
-        <div className={clsx(_CLASS_IS + '__top', classNames?.message)}>
+    <Container
+      className={clsx(_CLASS_IS, className, classNames?.container)}
+      style={style}
+    >
+      <Inner className={clsx(classNames?.inner, _CLASS_IS + "__inner")}>
+        <Top className={clsx(classNames?.top, _CLASS_IS + "__top")}>
           <AvatarGroup data={members} maxCount={2} size="large" />
-          <StackedText primaryText={title} secondaryText={date} className={clsx(_CLASS_IS + '__top__title')} />
-        </div>
-        <Message className={clsx(_CLASS_IS + '__message', classNames?.message)}>{message}</Message>
+          <Title
+            primaryText={title}
+            secondaryText={date}
+            className={clsx(classNames?.title, _CLASS_IS + "__top__title")}
+          />
+        </Top>
+        <Message className={clsx(classNames?.message, _CLASS_IS + "__message")}>
+          {message}
+        </Message>
       </Inner>
     </Container>
   );
--- stories/message-center/conversation-card.stories.tsx
+++ stories/message-center/conversation-card.stories.tsx
@@ -1,7 +1,15 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  Box as MuiBox,
+  Card as MuiCard,
+  CardContent as MuiCardContent,
+  Typography as MuiTypography,
+} from "@mui/material";
+import { Card as AntdCard, Typography as AntdTypography } from "antd";
+import { ConversationCard, StackedText } from "../../src";
 
-import { ConversationCard } from "../../src";
+import "antd/dist/antd.css";
 
 export default {
   title: "Roq Widgets/Message Center/Conversation Card",
@@ -9,6 +17,31 @@ export default {
   argTypes: {
     lastMessage: { control: "string" },
   },
+  decorators: [
+    (Story, context) => {
+      const [isLoaded, setIsLoaded] = useState(
+        context.name !== "With Tailwind"
+      );
+
+      useEffect(() => {
+        if (isLoaded) {
+          return;
+        }
+
+        const script = document.createElement("script");
+        script.onload = () => {
+          setIsLoaded(true);
+        };
+        script.src = "https://cdn.tailwindcss.com";
+        document.body.appendChild(script);
+        return () => {
+          // clean up effects of script here
+        };
+      }, []);
+
+      return isLoaded ? <Story /> : <div>Loading...</div>;
+    },
+  ],
 } as ComponentMeta<typeof ConversationCard>;
 
 const Template: ComponentStory<typeof ConversationCard> = (args) => (
@@ -28,4 +61,117 @@ Default.args = {
     { name: "Piper Wong", src: "https://i.pravatar.cc/60?img=14" },
     { name: "Jared Brewer", src: "https://i.pravatar.cc/60?img=12" },
   ],
+};
+
+const StyledTailwindText = (props) => {
+  return (
+    <StackedText
+      {...props}
+      classNames={{
+        ...props.classNames,
+        text: "font-semibold",
+        primaryText: "group-hover:text-blue-100",
+        secondaryText: "group-hover:text-blue-200",
+      }}
+    />
+  );
+};
+
+export const WithTailwind = Template.bind({});
+WithTailwind.args = {
+  title: "Chat",
+  date: "2 minutes ago",
+  message: `Hi! How it's going?`,
+  members: [
+    { name: "Mose Ewald", src: "https://i.pravatar.cc/60?img=15" },
+    { name: "Susan Gomez", src: "https://i.pravatar.cc/60?img=1" },
+    { name: "Piper Wong", src: "https://i.pravatar.cc/60?img=14" },
+    { name: "Jared Brewer", src: "https://i.pravatar.cc/60?img=12" },
+  ],
+  classNames: {
+    container:
+      "hover:bg-blue-500 hover:ring-blue-500 hover:shadow-md group rounded-md p-3 bg-white ring-1 ring-slate-200 shadow-sm",
+    message: "group-hover:text-white font-normal text-slate-900",
+  },
+  components: {
+    Title: StyledTailwindText,
+  },
+};
+
+const MuiTitle = (props) => {
+  return (
+    <StackedText
+      {...props}
+      components={{
+        ...props?.components,
+        primaryText: (p) => <MuiTypography {...p} variant="body1" />,
+        secondaryText: (p) => <MuiTypography {...p} variant="subtitle2" bold />,
+      }}
+    />
+  );
+};
+
+const MuiMessage = (props) => (
+  <MuiTypography {...props} variant="overline" display="block" gutterBottom />
+);
+
+const WithMuiCard = (props) => <MuiCard {...props} sx={{ padding: 0 }} />;
+
+export const WithMui = Template.bind({});
+WithMui.args = {
+  title: "Chat",
+  date: "2 minutes ago",
+  message: `Hi! How it's going?`,
+  theme: "none",
+  members: [
+    { name: "Mose Ewald", src: "https://i.pravatar.cc/60?img=15" },
+    { name: "Susan Gomez", src: "https://i.pravatar.cc/60?img=1" },
+    { name: "Piper Wong", src: "https://i.pravatar.cc/60?img=14" },
+    { name: "Jared Brewer", src: "https://i.pravatar.cc/60?img=12" },
+  ],
+  // style: { boxShadow: "none" },
+  components: {
+    Container: WithMuiCard,
+    Inner: MuiCardContent,
+    Top: MuiBox,
+    Title: MuiTitle,
+    Message: MuiMessage,
+  },
+};
+
+const AntdTitle = (props) => {
+  return (
+    <StackedText
+      {...props}
+      components={{
+        ...props?.components,
+        primaryText: (p) => <AntdTypography.Title {...p} level={5} />,
+        secondaryText: (p) => <AntdTypography.Text {...p} type="secondary" />,
+      }}
+    />
+  );
+};
+
+const AntdMessage = (props) => <AntdTypography.Text {...props} italic />;
+
+const WithAntdCard = (props) => <AntdCard {...props} hoverable />;
+
+export const WithAntd = Template.bind({});
+WithAntd.args = {
+  title: "Chat",
+  date: "2 minutes ago",
+  message: `Hi! How it's going?`,
+  theme: "none",
+  style: { boxShadow: "none" },
+  members: [
+    { name: "Mose Ewald", src: "https://i.pravatar.cc/60?img=15" },
+    { name: "Susan Gomez", src: "https://i.pravatar.cc/60?img=1" },
+    { name: "Piper Wong", src: "https://i.pravatar.cc/60?img=14" },
+    { name: "Jared Brewer", src: "https://i.pravatar.cc/60?img=12" },
+  ],
+  components: {
+    Container: WithAntdCard,
+    Title: AntdTitle,
+    Message: AntdMessage,
+  },
 };
